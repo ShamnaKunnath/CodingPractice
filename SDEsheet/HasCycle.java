5078c94b1459dcd6e53985ce5b9e26affe3456c9
shamnk <shamnakunnath54@gmail.com>
2024-09-23 07:41:15 +0530

    HasCycle in LL (slow-fast pointer approch)
    https://leetcode.com/problems/linked-list-cycle/

--- SDEsheet/HasCycle.java
+++ SDEsheet/HasCycle.java
@@ -12,6 +12,18 @@ class ListNode {
 
 public class Main {
 	public boolean hasCycle(ListNode head) {
+		ListNode slow = head, fast = head;
+		while(fast != null && fast.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            } 
+		}
+		return false;
+	}
+	public boolean hasCycleBruteForce(ListNode head) {
 		ListNode temp = head;
         Set<ListNode> nodeSet = new HashSet<>();
         while(temp != null){
