f110cc92b1da445fa77c7a81a0062e27bf20b75f
shamnk <shamnakunnath54@gmail.com>
2024-09-23 07:21:40 +0530

    Detect Cycle in LL Brute Force
    https://leetcode.com/problems/linked-list-cycle-ii/description/

---
+++ SDEsheet/HasCycle.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class ListNode {
+	int val;
+	ListNode next;
+	public ListNode(int val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+public class Main {
+	public boolean hasCycle(ListNode head) {
+		ListNode temp = head;
+        Set<ListNode> nodeSet = new HashSet<>();
+        while(temp != null){
+            if(nodeSet.contains(temp)) return true;
+            nodeSet.add(temp);
+            temp = temp.next;
+        }
+        return false;
+	}
+	public static void main(String[] args){
+		ListNode head = new ListNode(1);
+		ListNode second = new ListNode(2);
+		ListNode thrid = new ListNode(3);
+		ListNode fourth = new ListNode(4);
+		ListNode five = new ListNode(5);
+
+		//create list
+		head.next = second;
+		second.next = thrid;
+		third.next = four;
+		four.next = five;
+		
+		//created cycle
+		five.next = third;
+
+		if(hasCycle(head)){
+			System.out.println(Found cycle in list);
+		}
+		else {
+			System.out.println(Didn't find cycle in list);
+		}
+	}
+}
